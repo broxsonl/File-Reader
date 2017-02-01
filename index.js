a0d07214797157c6814d69d23647ec0a0185f62a
Lee Broxson <broxsonl@gmail.com>
2017-02-01 15:09:24 -0800

    Wrote module for longest word For funs, wrote a module for shortest word as well.

--- index.js
+++ index.js
@@ -6,13 +6,17 @@ const getWords = require('./get-words');
 // Methods to use with getWords
 const wordCount = require('./lib/word-count');
 const firstWordAlpha = require('./lib/first-word-alpha');
-
+const longestWord = require('./lib/longest-word');
+const shortestWord = require('./lib/shortest-word');
 
 const randomWord = require('./lib/random-word');
 
 // getWords(wordCount);
 // getWords(firstWordAlpha);
-getWords(randomWord);
+// getWords(longestWord);
+// getWords(shortestWord);
+
+// getWords(randomWord);
 
 
 //Write individual modules that can be required in here and passed in to getWords via process.argv
