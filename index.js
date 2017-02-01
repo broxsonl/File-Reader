43421bc95ecad2bcafb3f0114ecf6f04cdd12bf5
Lee Broxson <broxsonl@gmail.com>
2017-02-01 14:46:25 -0800

    Wrote a random word module.

--- index.js
+++ index.js
@@ -7,8 +7,12 @@ const getWords = require('./get-words');
 const wordCount = require('./lib/word-count');
 const firstWordAlpha = require('./lib/first-word-alpha');
 
+
+const randomWord = require('./lib/random-word');
+
 // getWords(wordCount);
-getWords(firstWordAlpha);
+// getWords(firstWordAlpha);
+getWords(randomWord);
 
 
 //Write individual modules that can be required in here and passed in to getWords via process.argv
