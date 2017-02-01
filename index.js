4384737049b5eab1da46062a47a9d61aebdaadf0
Lee Broxson <broxsonl@gmail.com>
2017-02-01 14:08:45 -0800

    Wrote module to log the first word alphabetically.

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 'use strict';
 
+// Basic method to get all of the words from the texts
 const getWords = require('./get-words');
-const wordCount = require('./lib/word-count')
 
-getWords(wordCount);
+// Methods to use with getWords
+const wordCount = require('./lib/word-count');
+const firstWordAlpha = require('./lib/first-word-alpha');
+
+// getWords(wordCount);
+getWords(firstWordAlpha);
 
 
 //Write individual modules that can be required in here and passed in to getWords via process.argv
