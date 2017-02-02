1b2036ddec8e3e15e4edcd0f48a61f064d9a3676
Lee Broxson <broxsonl@gmail.com>
2017-02-01 17:54:45 -0800

    Wrote module to find the word with the most unique letters.

--- index.js
+++ index.js
@@ -10,14 +10,20 @@ const longestWord = require('./lib/longest-word');
 const shortestWord = require('./lib/shortest-word');
 const countOfN = require('./lib/count-of-n');
 const mostLetterRepeats = require('./lib/most-letter-repeats');
+const mostUniqueLetters = require('./lib/most-unique-letters');
 const randomWord = require('./lib/random-word');
 
+let commands = []
+
+// let command = process.argv[2]
+// console.log(command);
 // getWords(wordCount);
 // getWords(firstWordAlpha);
 // getWords(longestWord);
 // getWords(shortestWord);
 // getWords(countOfN, 23);
-getWords(mostLetterRepeats);
+// getWords(mostLetterRepeats);
+// getWords(mostUniqueLetters);
 // getWords(randomWord);
 
 
